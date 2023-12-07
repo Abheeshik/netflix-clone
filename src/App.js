86d763f1ff52f83612d2c70bf864165ea871dd12
Abheeshik <2001abhishek13@gmail.com>
2023-12-07 12:25:03 +0530

    Fix Error

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
             <List title="Now Playing" param="now_playing"/>
             <List title="Popular" param="popular"/>
             <List title="Top Rated" param="top_rated"/>
-            <List title="Upcoming" param="upcoming"/>
+            <List title="Upcoming movies" param="upcoming"/>
           </React.Fragment>
           }/>
 
